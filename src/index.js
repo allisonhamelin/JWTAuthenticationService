86250d86b990404793ec1bc8b30f2579cff229e6
Valentin Prugnaud <valentin@whatdafox.com>
2018-10-26 22:35:16 -0700

    Create login endpoint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const app = express();
+
 const PORT = 8080;
+const JSON_SECRET_KEY = 'marvel';
+
+const users = [
+    { id: 1, username: 'bruceBanner', password: 'hulk' },
+    { id: 2, username: 'tonyStark', password: 'ironman' },
+    { id: 3, username: 'peterParker', password: 'spiderman' },
+];
+
+app.use(bodyParser.json());
 
 app.get('/time', (req, res) => {
     const time = (new Date()).toLocaleTimeString();
     res.status(200).send(`The Time is ${time}`);
 });
 
-app.get("*", (req, res) => {
+app.post('/login', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(400).send('Error. Please enter the correct username and password');
+        return;
+    }
+
+    const user = users.find((u) => {
+        return u.username === req.body.username && u.password === req.body.password;
+    });
+
+    const token = jwt.sign({
+        sub: user.id,
+        username: user.username
+    }, JSON_SECRET_KEY, { expiresIn: '3 hours' });
+
+    res.status(200).send({ token: token });
+});
+
+app.get('*', (req, res) => {
     res.sendStatus(404);
 });
 
